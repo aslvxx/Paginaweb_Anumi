40e93508ea08ce7ae6764569ab57cd62d0d8c272
David Vega <devc94@protonmail.com>
2026-06-28 21:34:14 -0600

    database config

--- Paginaweb_Anumi-api/app/main.py
+++ Paginaweb_Anumi-api/app/main.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from sqlmodel import SQLModel
+
+from app.database import engine
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Triggers table creation
+    SQLModel.metadata.create_all(engine)
+    yield
 
 app = FastAPI()
 
